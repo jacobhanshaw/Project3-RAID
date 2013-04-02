d7da3acbe2959fca4fc2e39d519275935ef33599
Jacob Hanshaw <hanshaw@wisc.edu>
2013-04-02 14:56:24 -0500

    Fixed bad block select code. Added input checks and case statements

--- main.c
+++ main.c
@@ -16,9 +16,10 @@ char * trace;
 int opt;
 disk_array_t da;
 
-void print_usage() {
-    printf("use: raidsim -level [0|10|4|5] -strip num -disks num -size num -trace filename [-verbose]\n");
-}
+void readRaid0 (int LBA, int SIZE);
+void writeRaid0 (int LBA, int SIZE, int VALUE);
+
+void print_usage();
 
 int main(int argc, char * argv[]) {
 
@@ -65,9 +66,20 @@ int main(int argc, char * argv[]) {
         }
     }	
 
+    //Could make sure blocks per strip is a multiple of blocks per disk, but not required
+    if((level != 0 && level != 10 && level != 4 && level != 5) ||
+            (level == 10 && ndisks % 2 != 0) ||
+            ((level == 4 || level == 5) && ndisks <= 2)){  
+        print_usage();
+        exit(1); 
+    }
+
     da = disk_array_create("myvirtualdisk", ndisks, nblocks);
 
+    //May technically be unnecessary as read/write returns -1 if disk has failed. However, it also returns -1 on other failures, but we probably don't have to worry about those.
+    //Remove if it causes problems
     int hasFailed[ndisks];
+    memset(hasFailed, 0, sizeof(int) *ndisks);
 
     FILE * fd = fopen(trace, "r");
     if (fd == NULL) {
@@ -98,13 +110,23 @@ int main(int argc, char * argv[]) {
             int LBA = atoi(strtok(NULL, " "));
             int SIZE = atoi(strtok(NULL, " "));
 
-            for(int i = LBA; i < LBA + SIZE; ++i)
-            {
-                if(hasFailed[(LBA % (ndisks*strip))/strip] || disk_array_read(da, (LBA % (ndisks*strip))/strip, LBA % strip, buffer) != 0) printf("ERROR");
-                else printf("%s", buffer);        
-                printf(" ");
+            switch(level){
+                case 0:
+                    readRaid0(LBA, SIZE);
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 10:
+
+                    break;
+                default:
+                    printf("ERROR: Reached impossible default block of read switch statement");
             }
-            printf("\n");
+
         } 
         else if (strcmp(cmd, "WRITE") == 0) 
         {
@@ -112,26 +134,70 @@ int main(int argc, char * argv[]) {
             int SIZE = atoi(strtok(NULL, " "));
             int VALUE = strtok(NULL, " ");
 
-            memset(buffer,VALUE,sizeof(buffer));
+            switch(level){
+                case 0:
+                    writeRaid0(LBA, SIZE, VALUE);
+                    break;
+                case 4:
 
-            for(int i = LBA; i < LBA + SIZE; ++i)
-            {
-                while(disk_array_write(da, (LBA % (ndisks*strip))/strip, LBA % strip, buffer) != 0) { } 
+                    break;
+                case 5:
+
+                    break;
+                case 10:
+
+                    break;
+                default:
+                    printf("ERROR: Reached impossible default block of write switch statement");
             }
+
         } 
         else if (strcmp(cmd, "FAIL") == 0) 
         {
             int DISK = atoi(strtok(NULL, " "));
             hasFailed[DISK] = 1;
             disk_array_fail_disk(da,DISK);
-            //Recalculate parity
+
+            switch(level){
+                case 0:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 10:
+
+                    break;
+                default:
+                    printf("ERROR: Reached impossible default block of fail switch statement");
+            }
+
         } 
         else if (strcmp(cmd, "RECOVER") == 0) 
         {
             int DISK = atoi(strtok(NULL, " "));
             hasFailed[DISK] = 0;
             disk_array_recover_disk(da,DISK);
-            //Rewrite values onto recovered disk and recalculate parity
+
+            switch(level){
+                case 0:
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 10:
+
+                    break;
+                default:
+                    printf("ERROR: Reached impossible default block of recover switch statement");
+            }
+
         } 
         else if (strcmp(cmd, "END") == 0) {
             disk_array_print_stats(da);
@@ -140,4 +206,27 @@ int main(int argc, char * argv[]) {
     }
     free(line);
     return 0; 
+}
+
+void readRaid0 (int LBA, int SIZE){
+    for(int i = LBA; i < LBA + SIZE; ++i)
+    {
+        if(hasFailed[(LBA % (ndisks*strip))/strip] || disk_array_read(da, (LBA % (ndisks*strip))/strip, (LBA/(strip * ndisks)) * strip + LBA % strip, buffer) != 0) printf("ERROR");
+        else printf("%s", buffer);        
+        printf(" ");
+    }
+    printf("\n");
+}
+
+void writeRaid0 (int LBA, int SIZE, int VALUE){
+    memset(buffer,VALUE,sizeof(buffer));
+
+    for(int i = LBA; i < LBA + SIZE; ++i)
+    {
+        if(hasFailed[(LBA % (ndisks*strip))/strip] || disk_array_write(da, (LBA % (ndisks*strip))/strip, (LBA/(strip * ndisks)) * strip + LBA % strip, buffer) != 0) { } //FIX: Do something here
+    }
+} 
+
+void print_usage() {
+    printf("use: raidsim -level [0|10|4|5] -strip num -disks num -size num -trace filename [-verbose]\n");
 }
